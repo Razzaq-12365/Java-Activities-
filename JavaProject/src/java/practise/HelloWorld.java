5304e173b8f5b52536724cda9b07cf59a0d74cb0
Syed Rahmath <Syed Rahmath@LAPTOP-K48TLV76>
2022-12-14 16:17:49 +0530

    Initial commit

---
+++ JavaProject/src/java/practise/HelloWorld.java
@@ -0,0 +1,10 @@
+package java.practise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ JavaProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Syed Rahmath
+ *
+ */
+module JavaProject {
+}
